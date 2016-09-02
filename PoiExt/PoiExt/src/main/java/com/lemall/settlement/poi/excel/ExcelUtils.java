62b7eba19d568474a7a7e95801ce7f85a26dc135
heyongcheng <heyongcheng@CNBJWK5282SOP.letv.local>
2016-09-02 14:35:24 +0800

    增加导出方法

--- PoiExt/PoiExt/src/main/java/com/lemall/settlement/poi/excel/ExcelUtils.java
+++ PoiExt/PoiExt/src/main/java/com/lemall/settlement/poi/excel/ExcelUtils.java
@@ -2,7 +2,11 @@ package com.lemall.settlement.poi.excel;
 
 import java.io.ByteArrayOutputStream;
 
+import org.apache.commons.lang3.StringUtils;
+
+import com.lemall.settlement.poi.configure.ConfigContext;
 import com.lemall.settlement.poi.configure.model.Config;
+import com.lemall.settlement.poi.exception.ExcelHandleException;
 import com.lemall.settlement.poi.progressor.Progressor;
 
 /**
@@ -20,6 +24,24 @@ public class ExcelUtils {
 	 * @return
 	 */
 	public static ByteArrayOutputStream createExcel(Object data,Config config,Progressor progressor){
+		return null;
+	}
+	/**
+	 * 生成Excel文件
+	 * @param data
+	 * @param configId
+	 * @param progressor
+	 * @return
+	 */
+	public static ByteArrayOutputStream createExcel(Object data,String configId,Progressor progressor){
+		if(StringUtils.isEmpty(configId)){
+			throw new ExcelHandleException("config id can not be null");
+		}
+		Config config = ConfigContext.CONFIG_MAP.get(configId);
+		if(config == null){
+			throw new ExcelHandleException("can not find excel config:" + configId);
+		}
+		
 		return null;
 	}
 }
